57517c9b3bbff4e72c3ac27e1e6d8dbc13f47ddb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-17 21:50:38 -0500

    docs: Add module-level docstring and comments
    - Added a module-level docstring providing an overview of the module's purpose.
    - Added comments throughout the code to explain the purpose of custom types and error handling.
    - Improved code readability and understanding.
    
    These updates enhance the documentation and readability of the source file for better code comprehension.

--- jsonpycraft/core/types.py
+++ jsonpycraft/core/types.py
@@ -1,25 +1,38 @@
 """
 jsonpycraft/core/types.py
+
+This module defines custom types and error handling related to JSON data.
+
+- JSONMap: A dictionary with string keys and values of any type.
+- JSONList: A list of dictionaries, where each dictionary has string keys and values of any type.
+- JSONData: A union type representing JSONMap or JSONList for flexibility in JSON data representation.
+
+- EncodeError: Error types for JSON encoding, including TypeError (raised by json.dump(s)) and file-related errors.
+- DecodeError: Error types for JSON decoding, including json.JSONDecodeError and file-related errors.
+- JSONError: A unified set of error types for handling JSON-related errors.
+
+Usage:
+- Import this module to use the custom types and error handling in your code.
 """
+
 import json
 from typing import Any, Dict, List, Union
 
-# NOTE:
-# List[Dict[str, Any]] uses a data type value of Any,
-# enforcing generic types, to allow for portability.
+# Custom Types Definitions
 JSONMap = Dict[str, Any]
 JSONList = List[JSONMap]
 JSONData = Union[JSONMap, JSONList]
 
+# Error Definitions
 EncodeError = (
-    TypeError,  # raised by json.dump(s)
+    TypeError,  # Raised by json.dump(s)
     FileNotFoundError,
     NotADirectoryError,
     PermissionError,
 )
 
 DecodeError = (
-    json.JSONDecodeError,  # raised by json.load(s)
+    json.JSONDecodeError,  # Raised by json.load(s)
     FileNotFoundError,
     NotADirectoryError,
     PermissionError,
