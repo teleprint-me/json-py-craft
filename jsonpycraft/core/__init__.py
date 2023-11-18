40d495908a016b1430aa7bc71ec51074488af52d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-18 00:19:16 -0500

    core: Add __init__.py module for jsonpycraft.core
    - Introduces an initializer module for the jsonpycraft.core package.
    - Imports key components including logger, Singleton, and custom types/errors.
    - Provides convenient access to essential functionality within the package.

---
+++ jsonpycraft/core/__init__.py
@@ -0,0 +1,13 @@
+"""
+jsonpycraft/core/__init__.py
+"""
+from jsonpycraft.core.logger import get_default_logger
+from jsonpycraft.core.singleton import Singleton
+from jsonpycraft.core.types import (
+    DecodeError,
+    EncodeError,
+    JSONData,
+    JSONError,
+    JSONList,
+    JSONMap,
+)
