b34ccdc4c7d64f3be903678f2766e4eb65d8144f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-22 22:49:50 -0500

    test: Add tests for json/io.py functions
    - Added tests for the read_json, dump_json, write_json, and force_read_json functions.
    - Modified the test_dump_json to expect an indentation of 2 spaces in the dumped JSON string.

---
+++ tests/json/test_io.py
@@ -0,0 +1,32 @@
+"""
+tests/json/test_io.py
+"""
+import json
+import os
+
+from jsonpycraft.json.io import dump_json, force_read_json, read_json, write_json
+
+
+class TestJsonFunctions:
+    def setup_method(self):
+        self.file_path = "tests/test.temp.json"
+        self.data = {"test": "data"}
+        write_json(self.file_path, self.data)
+
+    def teardown_method(self):
+        # Clean up after each test by removing the file
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
+    def test_read_json(self):
+        assert read_json(self.file_path) == self.data
+
+    def test_dump_json(self):
+        assert dump_json(self.file_path) == json.dumps(self.data, indent=2)
+
+    def test_write_json(self):
+        assert read_json(self.file_path) == self.data
+
+    def test_force_read_json(self):
+        assert force_read_json(self.file_path, self.data) == self.data
+        assert read_json(self.file_path) == self.data
