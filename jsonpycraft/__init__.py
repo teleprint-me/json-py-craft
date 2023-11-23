34ea9ce2f907323d4486eeef6ccca828e8c1b7b6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-22 23:08:16 -0500

    feat: Add project details to __init__.py
    - Extracted project details such as name, version, description, author, license, and readme from pyproject.toml and added them to the __init__.py file for better project context within the codebase.

--- jsonpycraft/__init__.py
+++ jsonpycraft/__init__.py
@@ -16,4 +16,18 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Project Details:
+- Name: jsonpycraft
+- Version: 0.1.0
+- Description: JsonPyCraft is a specialized Python toolkit designed for efficient and structured JSON management.
+- Author: Austin Berrio <aberrio@teleprint.me>
+- License: AGPL
+- Readme: README.md
 """
+
+# Additional project details extracted from pyproject.toml
+__version__ = "0.1.0"
+__author__ = "Austin Berrio <aberrio@teleprint.me>"
+__description__ = "JsonPyCraft is a specialized Python toolkit designed for efficient and structured JSON management."
+__license__ = "AGPL"
