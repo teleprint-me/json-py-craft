3a68681795aabe05ab2ef40a2fd6bcf8486860ed
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-22 22:38:08 -0500

    feat: Added functions from jsonpycraft.json.io to __init__.py
    - Updated the __init__.py file to include functions for reading, writing, and manipulating JSON data from the jsonpycraft.json.io module.
    
    This change makes it easier for users to access these functions when using the jsonpycraft library.

---
+++ jsonpycraft/json/__init__.py
@@ -0,0 +1,7 @@
+"""
+jsonpycraft/json/__init__.py
+"""
+from jsonpycraft.json.base import JSONBaseTemplate
+from jsonpycraft.json.io import dump_json, force_read_json, read_json, write_json
+from jsonpycraft.json.list import JSONListTemplate
+from jsonpycraft.json.map import JSONMapTemplate
