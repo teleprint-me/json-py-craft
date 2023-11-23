92b96d9538f7cc8d0a2cf267bfb5969f9d4e7a62
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-22 22:53:31 -0500

    tests/json/test_io.py: Simplify tests and use fixture
    - Removed the `TestJsonFunctions` class and simplified the tests using individual test functions.
    - Introduced a fixture `temp_json_file` to create a temporary JSON file for testing.
    - Updated the tests to use the fixture and removed the need for a teardown method.
    - Ensured that the tests still pass successfully.

--- tests/json/test_io.py
+++ tests/json/test_io.py
@@ -4,29 +4,33 @@ tests/json/test_io.py
 import json
 import os
 
+import pytest
+
 from jsonpycraft.json.io import dump_json, force_read_json, read_json, write_json
 
 
-class TestJsonFunctions:
-    def setup_method(self):
-        self.file_path = "tests/test.temp.json"
-        self.data = {"test": "data"}
-        write_json(self.file_path, self.data)
+@pytest.fixture
+def temp_json_file(tmp_path):
+    file_path = tmp_path / "test.temp.json"
+    data = {"test": "data"}
+    write_json(file_path, data)
+    yield file_path
+    if file_path.exists():
+        file_path.unlink()
 
-    def teardown_method(self):
-        # Clean up after each test by removing the file
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
 
-    def test_read_json(self):
-        assert read_json(self.file_path) == self.data
+def test_read_json(temp_json_file):
+    assert read_json(temp_json_file) == {"test": "data"}
 
-    def test_dump_json(self):
-        assert dump_json(self.file_path) == json.dumps(self.data, indent=2)
 
-    def test_write_json(self):
-        assert read_json(self.file_path) == self.data
+def test_dump_json(temp_json_file):
+    assert dump_json(temp_json_file) == json.dumps({"test": "data"}, indent=2)
 
-    def test_force_read_json(self):
-        assert force_read_json(self.file_path, self.data) == self.data
-        assert read_json(self.file_path) == self.data
+
+def test_write_json(temp_json_file):
+    assert read_json(temp_json_file) == {"test": "data"}
+
+
+def test_force_read_json(temp_json_file):
+    assert force_read_json(temp_json_file, {"another": "data"}) == {"test": "data"}
+    assert read_json(temp_json_file) == {"test": "data"}
