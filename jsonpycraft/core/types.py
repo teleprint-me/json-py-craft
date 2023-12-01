adc2f576f50e99b96103834086d0dd4972c6b38b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-30 21:10:48 -0500

    refactor: Expand Error Handling and Improve Code Readability
    - Introduced "Custom Types" and "File Handling Error Types" section headers for better organization.
    - Added new `FileError` type encompassing file-related errors, including FileNotFoundError, NotADirectoryError, PermissionError, IsADirectoryError, and IOError.
    - Redefined `EncodeError` and `DecodeError` as tuples, including TypeError and JSONDecodeError, respectively, along with file-related errors from `FileError`.
    - Maintained `JSONError` as a unified set for both encoding and decoding errors.
    - Optimized imports by changing from `import json` to `from json import JSONDecodeError`.
    - Added explanatory comments for `TypeError` and `JSONDecodeError` to clarify their usage.
    
    These changes enhance error handling and improve the readability and organization of the types.py module.

--- jsonpycraft/core/types.py
+++ jsonpycraft/core/types.py
@@ -3,19 +3,26 @@ jsonpycraft/core/types.py
 
 This module defines custom types and error handling related to JSON data.
 
+Custom Types:
 - JSONMap: A dictionary with string keys and values of any type.
 - JSONList: A list of dictionaries, where each dictionary has string keys and values of any type.
 - JSONData: A union type representing JSONMap or JSONList for flexibility in JSON data representation.
 
-- EncodeError: Error types for JSON encoding, including TypeError (raised by json.dump(s)) and file-related errors.
-- DecodeError: Error types for JSON decoding, including json.JSONDecodeError and file-related errors.
-- JSONError: A unified set of error types for handling JSON-related errors.
+File Handling Error Types:
+- FileError: A tuple of file-related error types, including FileNotFoundError, NotADirectoryError, PermissionError, IsADirectoryError, and IOError.
+
+Custom Error Types for JSON Encoding and Decoding:
+- EncodeError: A tuple of error types for JSON encoding, including TypeError (raised by json.dump(s)) and file-related errors.
+
+- DecodeError: A tuple of error types for JSON decoding, including json.JSONDecodeError (raised by json.loads) and file-related errors.
+
+- JSONError: A unified set of error types for handling JSON-related errors, including both EncodeError and DecodeError.
 
 Usage:
 - Import this module to use the custom types and error handling in your code.
 """
 
-import json
+from json import JSONDecodeError
 from typing import Any, Dict, List, Union
 
 # Custom Types Definitions
@@ -23,19 +30,20 @@ JSONMap = Dict[str, Any]
 JSONList = List[JSONMap]
 JSONData = Union[JSONMap, JSONList]
 
-# Error Definitions
-EncodeError = (
-    TypeError,  # Raised by json.dump(s)
+# File Handling Error Definitions
+FileError = (
     FileNotFoundError,
     NotADirectoryError,
     PermissionError,
+    IsADirectoryError,  # For handling directories where a file is expected
+    IOError,  # For general I/O errors
 )
 
-DecodeError = (
-    json.JSONDecodeError,  # Raised by json.load(s)
-    FileNotFoundError,
-    NotADirectoryError,
-    PermissionError,
-)
+# TypeError is raised by json.dumps for invalid JSON types
+EncodeError = (TypeError,) + FileError
 
+# JSONDecodeError is raised by json.loads for invalid JSON formatting
+DecodeError = (JSONDecodeError,) + FileError
+
+# Custom Error Definitions for JSON Encoding and Decoding
 JSONError = EncodeError + DecodeError
