c6052fa3d48de8f06421030194349726bb7497cb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-30 21:33:22 -0500

    refactor: Update core __init__.py Imports
    - Removed import of `get_default_logger` from `jsonpycraft.core.logger`
    - Added import of `FileError` to the `jsonpycraft.core.types` imports
    - Streamlined the import list to align with recent changes in the core module
    
    This update ensures that the core subpackage's __init__.py file reflects the latest modifications in error handling and core functionalities.

--- jsonpycraft/core/__init__.py
+++ jsonpycraft/core/__init__.py
@@ -1,11 +1,11 @@
 """
 jsonpycraft/core/__init__.py
 """
-from jsonpycraft.core.logger import get_default_logger
 from jsonpycraft.core.singleton import Singleton
 from jsonpycraft.core.types import (
     DecodeError,
     EncodeError,
+    FileError,
     JSONData,
     JSONError,
     JSONList,
