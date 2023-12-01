bad30eeba731432df349f490b9a2900360f0a508
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-11-30 22:57:26 -0500

    feat: Integrate Custom Exceptions into Core Package
    - Updated `core/__init__.py` to import the new custom exceptions: `JSONDecodeErrorHandler`, `JSONEncodeErrorHandler`, `JSONFileErrorHandler`
    - Ensured easy access to these exceptions throughout the JSONPyCraft project by adding them to the core package's init file
    - The update promotes better error handling practices and code modularity within the core package
    
    This enhancement facilitates the use of custom exceptions across the JSONPyCraft project, contributing to a more structured and maintainable codebase.

--- jsonpycraft/core/__init__.py
+++ jsonpycraft/core/__init__.py
@@ -1,6 +1,11 @@
 """
 jsonpycraft/core/__init__.py
 """
+from jsonpycraft.core.errors import (
+    JSONDecodeErrorHandler,
+    JSONEncodeErrorHandler,
+    JSONFileErrorHandler,
+)
 from jsonpycraft.core.singleton import Singleton
 from jsonpycraft.core.types import (
     DecodeError,
