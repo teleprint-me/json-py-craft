017c59d906de49f06c131907e14e03f4421ac2c2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-02 11:25:55 -0500

    test: Enhance JSONBaseTemplate Error Handling Coverage
    - Added test to verify immutability of file_path attribute
    - Implemented tests for loading nonexistent and corrupted JSON files
    - Created test for saving data with incorrect data type (non-serializable set)
    - Added test for backup creation in a directory with insufficient permissions
    - Included test for directory creation where a conflicting non-directory file exists
    - Ensured proper cleanup and resource management in tests
    
    These tests enhance the robustness of error handling in JSONBaseTemplate by covering a wider range of scenarios, including edge cases and typical error conditions.

--- tests/json/test_base.py
+++ tests/json/test_base.py
@@ -34,6 +34,8 @@ def json_base_template(tmp_path):
 def test_json_file_path(json_base_template):
     assert json_base_template.file_path is not None
     assert isinstance(json_base_template.file_path, Path)
+    with pytest.raises(AttributeError):  # file path is immutable
+        json_base_template.file_path = "tests/error.json"
 
 
 def test_json_data(json_base_template):
@@ -77,3 +79,66 @@ def test_json_directory_creation(json_base_template, tmp_path):
         dir_path.rmdir()  # Ensure the directory doesn't exist before the test
     json_base_template.make_directory()
     assert dir_path.exists() is True
+
+
+def test_loading_nonexistent_json_file():
+    json_template = JSONBaseTemplate("nonexistent.json")
+
+    # test loading
+    with pytest.raises(JSONFileErrorHandler):
+        json_template.load_json()
+
+    # test backing up loaded file
+    with pytest.raises(JSONFileErrorHandler):
+        json_template.backup_json()
+
+
+def test_saving_with_incorrect_data_type(json_base_template, tmp_path):
+    new_data = {"new", "data", "set"}
+    with pytest.raises(JSONEncodeErrorHandler):
+        json_base_template.save_json(new_data)
+
+
+def test_loading_corrupted_json_file(tmp_path):
+    temp_json_file = tmp_path / "corrupted.json"
+    # Write invalid JSON data (missing closing brace)
+    temp_json_file.write_text('{"key": "value"')
+
+    json_template = JSONBaseTemplate(str(temp_json_file))
+    with pytest.raises(JSONDecodeErrorHandler):
+        json_template.load_json()
+
+
+def test_backup_creation_insufficient_permissions(tmp_path):
+    restricted_dir = tmp_path / "restricted_dir"
+    os.mkdir(restricted_dir)
+    original_permissions = os.stat(restricted_dir).st_mode  # Store original permissions
+    try:
+        os.chmod(restricted_dir, 0o000)  # Restrict write permissions
+
+        json_base_template = JSONBaseTemplate(
+            str(restricted_dir / "test.json"),
+            initial_data={"key": "value"},
+        )
+
+        with pytest.raises(JSONFileErrorHandler):
+            json_base_template.save_json()
+            json_base_template.backup_json()
+    finally:
+        os.chmod(restricted_dir, original_permissions)  # Reset permissions
+
+
+def test_directory_creation_with_existing_file(tmp_path):
+    # Create a file with the same name as the intended directory
+    conflicting_file = tmp_path / "conflicting_name"
+    conflicting_file.touch()  # This creates an empty file
+
+    # Set the JSONBaseTemplate to use a path where the last component is this file
+    json_template = JSONBaseTemplate(str(conflicting_file / "test.json"))
+
+    with pytest.raises(JSONFileErrorHandler):
+        json_template.make_directory()
+
+    # Cleanup after the test
+    if conflicting_file.exists():
+        conflicting_file.unlink()
