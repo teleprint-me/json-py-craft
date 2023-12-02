dfb688b5bdc373296407534a59cedd729ad7b54a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-02 18:00:32 -0500

    refactor: Update JSON IO Module with Enhanced Error Handling and Documentation
    - Corrected module description to match actual module name in the docstring.
    - Included custom error handlers from `jsonpycraft.core.errors` for specific error reporting.
    - Added `FileError` to the import list for broader file-related error handling.
    - Enhanced function descriptions for clarity in `read_json`, `dump_json`, and `write_json`.
    - Refined argument descriptions in docstrings to specify the nature and purpose of each parameter.
    - Updated return value descriptions to accurately reflect the outcomes of functions.
    - Replaced generic `DecodeError` and `EncodeError` with custom error handlers for more specific error reporting.
    - Improved the clarity of the `force_read_json` function documentation and error handling approach.
    
    These changes improve the module's readability, error specificity, and overall documentation, making it more user-friendly and easier to maintain.

--- jsonpycraft/json/io.py
+++ jsonpycraft/json/io.py
@@ -1,97 +1,112 @@
 """
-jsonpycraft/json/utils.py
+jsonpycraft/json/io.py
 """
+
 import json
 from pathlib import Path
 from typing import Union
 
-from jsonpycraft.core.types import DecodeError, EncodeError, JSONData
+from jsonpycraft.core.errors import (
+    JSONDecodeErrorHandler,
+    JSONEncodeErrorHandler,
+    JSONFileErrorHandler,
+)
+from jsonpycraft.core.types import DecodeError, EncodeError, FileError, JSONData
 
 
 def read_json(filepath: Union[str, Path]) -> JSONData:
     """
-    Read JSON data from a file.
+    Reads JSON data from a file.
 
     Args:
-        filepath (Union[str, Path]): The path to the JSON file.
+        filepath (Union[str, Path]): The path to the JSON file to read.
 
     Returns:
-        JSONData: The deserialized JSON data.
+        JSONData: The JSON data read from the file.
 
     Raises:
-        DecodeError: If there is an error reading or decoding the JSON data.
+        JSONFileErrorHandler: If a file-related error occurs.
+        JSONDecodeErrorHandler: If a JSON decoding error occurs.
     """
     try:
         with open(filepath, "r") as file:
             return json.load(file)
+    except FileError as e:
+        raise JSONFileErrorHandler(f"File error accessing {filepath}: {e}")
     except DecodeError as e:
-        raise DecodeError(f"Error decoding JSON data at {filepath}: {e}")
+        raise JSONDecodeErrorHandler(f"Error decoding JSON data at {filepath}: {e}")
 
 
 def dump_json(filepath: Union[str, Path], indent: int = 2) -> str:
     """
-    Serialize JSON data to a formatted string.
+    Reads JSON data from a file, dumps it as a formatted JSON string, and returns it.
 
     Args:
-        filepath (Union[str, Path]): The path to the JSON file.
-        indent (int): The number of spaces to use for indentation in the output. Defaults to 2.
+        filepath (Union[str, Path]): The path to the JSON file to read.
+        indent (int): The indentation level for the formatted JSON string (default is 2).
 
     Returns:
-        str: The serialized JSON data in a formatted string.
+        str: The formatted JSON data as a string.
 
     Raises:
-        EncodeError: If there is an error encoding and writing the JSON data.
+        JSONFileErrorHandler: If a file-related error occurs.
+        JSONEncodeErrorHandler: If a JSON encoding error occurs.
     """
     try:
         with open(filepath, "r") as file:
             return json.dumps(json.load(file), indent=indent)
+    except FileError as e:
+        raise JSONFileErrorHandler(f"File error accessing {filepath}: {e}")
     except EncodeError as e:
-        raise EncodeError(f"Error encoding JSON data at {filepath}: {e}")
+        raise JSONEncodeErrorHandler(f"Error encoding JSON data at {filepath}: {e}")
 
 
 def write_json(filepath: Union[str, Path], content: JSONData, indent: int = 2) -> None:
     """
-    Write JSON data to a file.
+    Writes JSON data to a file.
 
     Args:
-        filepath (Union[str, Path]): The path to the JSON file.
-        content (JSONData): The data to be serialized and written as JSON.
-        indent (int, optional): The number of spaces to use for indentation. Defaults to 2.
+        filepath (Union[str, Path]): The path to the JSON file to write.
+        content (JSONData): The JSON data to write to the file.
+        indent (int): The indentation level for the formatted JSON (default is 2).
 
     Raises:
-        EncodeError: If there is an error encoding and writing the JSON data.
+        JSONFileErrorHandler: If a file-related error occurs.
+        JSONEncodeErrorHandler: If a JSON encoding error occurs.
     """
     try:
         with open(filepath, "w") as f:
             json.dump(content, f, indent=indent)
+    except FileError as e:
+        raise JSONFileErrorHandler(f"File error accessing {filepath}: {e}")
     except EncodeError as e:
-        raise EncodeError(f"Error encoding and writing JSON data to {filepath}: {e}")
+        raise JSONEncodeErrorHandler(
+            f"Error encoding and writing JSON data to {filepath}: {e}"
+        )
 
 
 def force_read_json(
     filepath: Union[str, Path], content: JSONData, indent: int = 2
 ) -> JSONData:
     """
-    Read JSON data from a file, or create the file with default content if it doesn't exist.
-
-    If the specified file does not exist, it will be created with the provided content.
-    This is useful for ensuring that a file exists before reading from it.
+    Reads JSON data from a file or writes default content if decoding fails.
 
     Args:
-        filepath (Union[str, Path]): The path to the JSON file.
-        content (JSONData): The default content to be written as JSON if the file doesn't exist.
-        indent (int, optional): The number of spaces to use for indentation. Defaults to 2.
+        filepath (Union[str, Path]): The path to the JSON file to read or write.
+        content (JSONData): The default JSON data to write if reading fails.
+        indent (int): The indentation level for the formatted JSON (default is 2).
 
     Returns:
-        JSONData: The deserialized JSON data.
+        JSONData: The JSON data read from the file or default content if reading fails.
 
     Raises:
-        DecodeError: If there is an error decoding the JSON data.
-        EncodeError: If there is an error encoding and writing the JSON data.
+        JSONFileErrorHandler: If a file-related error occurs.
+        JSONEncodeErrorHandler: If a JSON encoding error occurs.
     """
     try:
         return read_json(filepath)
-    except DecodeError:
-        # If the file does not exist or cannot be decoded, write the default content.
+    except JSONDecodeErrorHandler:
+        # If a JSONDecodeErrorHandler occurs (e.g., JSON decoding error),
+        # write the default content and return it.
         write_json(filepath, content, indent=indent)
         return content
