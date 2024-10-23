a5a3dd748dfaacf56eaec6dd4a0dba7659329d3b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-22 23:21:57 -0400

    sh(chore): Exit on failure and be verbose upon execution

--- sh/build.sh
+++ sh/build.sh
@@ -2,7 +2,11 @@
 
 # sh/build.sh
 # build for production
+set -e  # Exit immediately if a command exits with a non-zero status
 
+echo "Building the package..."
 poetry build
+echo "Publishing to TestPyPI..."
 poetry publish --build --repository pypi
+echo "Installing from PyPI..."
 pip install jsonpycraft
--- sh/test_build.sh
+++ sh/test_build.sh
@@ -2,7 +2,11 @@
 
 # sh/test_build.sh
 # build for testing
+set -e  # Exit immediately if a command exits with a non-zero status
 
+echo "Building the package..."
 poetry build
+echo "Publishing to TestPyPI..."
 poetry publish --build --repository testpypi
+echo "Installing from TestPyPI..."
 pip install --index-url https://test.pypi.org/simple/ jsonpycraft
